b793cb349603d767e0517b5ab8a80f41f54c15d1
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-11-12 23:37:43 +0200

    working state

--- src/main/java/com/example/candidate/CandidateApplication.java
+++ src/main/java/com/example/candidate/CandidateApplication.java
@@ -2,9 +2,11 @@ package com.example.candidate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-@SpringBootApplication
+@SpringBootApplication()
 @EnableDiscoveryClient
 public class CandidateApplication {
 
