c970fd3346107d6e1c79a3564db269b93b784b3e
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2023-11-14 23:41:13 +0200

    removed filters for custom JWT

--- src/main/java/com/example/candidate/CandidateApplication.java
+++ src/main/java/com/example/candidate/CandidateApplication.java
@@ -2,11 +2,8 @@ package com.example.candidate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-@SpringBootApplication()
+@SpringBootApplication
 @EnableDiscoveryClient
 public class CandidateApplication {
 
