9fbe8a12dffae2d087aeef175651f428fed75971
SerbanTiurbe21 <serban.tiurbe@student.upt.ro>
2024-04-04 16:46:48 +0300

    - updated ReactiveSecurityConfig.java

--- src/main/java/com/example/candidate/config/security/ReactiveSecurityConfig.java
+++ src/main/java/com/example/candidate/config/security/ReactiveSecurityConfig.java
@@ -19,7 +19,7 @@ public class ReactiveSecurityConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-                .csrf().disable()
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers("/eureka/**").permitAll()
                         .anyExchange().authenticated()
